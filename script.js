6e6e5c14324093ec90348c55fb59701b83d4ca3a
canna88 <97223496+canna88@users.noreply.github.com>
2023-09-02 18:09:48 +0200

    nav-bar-complete

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchIcon = document.querySelector('.bi-search');
+    const searchInput = document.querySelector('.form-control');
+  
+    // Aggiunge un gestore di eventi al clic sull'icona di ricerca
+    searchIcon.addEventListener('click', function () {
+      searchInput.classList.add('active');
+      searchInput.focus();
+    });
+  
+    // Rimuove la classe "active" quando si perde il focus
+    searchInput.addEventListener('blur', function () {
+      searchInput.classList.remove('active');
+    });
+  });
+  
+
+
+
+
+
+
+
